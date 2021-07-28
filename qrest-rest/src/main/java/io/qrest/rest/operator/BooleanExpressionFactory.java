bf60138d784979e39e5f7ddd3e940377dded7b48
Martin Schäfer <martin.schaefer.14@outlook.com>
2021-07-28 21:47:08 +0200

    BooleanExpressionFactoryTest is green

--- qrest-rest/src/main/java/io/qrest/rest/operator/BooleanExpressionFactory.java
+++ qrest-rest/src/main/java/io/qrest/rest/operator/BooleanExpressionFactory.java
@@ -1,5 +1,14 @@
 package io.qrest.rest.operator;
 
+import static io.qrest.rest.operator.OperatorSymbols.EQUAL;
+import static io.qrest.rest.operator.OperatorSymbols.IN;
+import static io.qrest.rest.operator.OperatorSymbols.LIKE;
+import static io.qrest.rest.operator.OperatorSymbols.LIKE_CASE_INSENSITIVE;
+import static io.qrest.rest.operator.OperatorSymbols.NOT_EQUAL;
+import static io.qrest.rest.operator.OperatorSymbols.NOT_IN;
+import static io.qrest.rest.operator.OperatorSymbols.NULL_VALUE;
+import static org.apache.commons.lang3.StringUtils.replace;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -9,6 +18,7 @@ import org.springframework.core.convert.ConversionService;
 import com.querydsl.core.types.Path;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 
 import cz.jirutka.rsql.parser.ast.ComparisonOperator;
 import lombok.RequiredArgsConstructor;
@@ -16,34 +26,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BooleanExpressionFactory {
 
-	private static final String EQ = "==";
-	private static final String NEQ = "!=";
-	private static final String IN = "=in=";
-	private static final String OUT = "=out=";
-	private static final String NULL = "null";
 	private final ConversionService conversionService;
 
 	public BooleanExpression create(Path<?> path, String operatorSymbol, List<String> args) {
 		// one value is guaranteed
 		String arg = args.get(0);
+		if (path instanceof StringExpression) {
+			StringExpression se = (StringExpression) path;
+			switch (operatorSymbol) {
+			case LIKE:
+				return se.like(replace(arg, "*", "%"));
+			case LIKE_CASE_INSENSITIVE:
+				return se.likeIgnoreCase(replace(arg, "*", "%"));
+			}
+		}
 		if (path instanceof SimpleExpression<?>) {
 			SimpleExpression<Object> se = (SimpleExpression<Object>) path;
 			switch (operatorSymbol) {
-			case EQ:
-				if (NULL.equals(arg)) {
+			case EQUAL:
+				if (NULL_VALUE.equals(arg)) {
 					return se.isNull();
 				} else {
 					return se.eq(convert(arg, path));
 				}
-			case NEQ:
-				if (NULL.equals(arg)) {
+			case NOT_EQUAL:
+				if (NULL_VALUE.equals(arg)) {
 					return se.isNotNull();
 				} else {
 					return se.ne(convert(arg, path));
 				}
 			case IN:
 				return se.in(convert(args, path));
-			case OUT:
+			case NOT_IN:
 				return se.notIn(convert(args, path));
 			}
 		}
--- qrest-rest/src/test/java/io/qrest/rest/operator/BooleanExpressionFactoryTest.java
+++ qrest-rest/src/test/java/io/qrest/rest/operator/BooleanExpressionFactoryTest.java
@@ -1,11 +1,19 @@
 package io.qrest.rest.operator;
 
+import static io.qrest.rest.operator.OperatorSymbols.EQUAL;
+import static io.qrest.rest.operator.OperatorSymbols.IN;
+import static io.qrest.rest.operator.OperatorSymbols.LIKE;
+import static io.qrest.rest.operator.OperatorSymbols.LIKE_CASE_INSENSITIVE;
+import static io.qrest.rest.operator.OperatorSymbols.NOT_EQUAL;
+import static io.qrest.rest.operator.OperatorSymbols.NOT_IN;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.Mockito.mock;
 
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
@@ -13,8 +21,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.BDDMockito;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.convert.ConversionService;
 
@@ -27,7 +33,8 @@ public class BooleanExpressionFactoryTest {
 	@ParameterizedTest
 	@MethodSource("operatorSymbolSource")
 	public void stringPathExpressions_withValidOperators_ShouldBeCreated(String operatorSymbol,
-			MockFunction<StringPath, String, BooleanExpression> mockFunction) {
+			MockFunction<StringPath, String, BooleanExpression> mockFunction,
+			BiConsumer<StringPath, String> verifyFunction) {
 
 		// given
 		StringPath stringPath = mock(StringPath.class);
@@ -35,7 +42,6 @@ public class BooleanExpressionFactoryTest {
 		ConversionService conversionService = mock(ConversionService.class);
 		String value = StringUtils.replace(operatorSymbol, "=", "abc");
 		mockFunction.mock(stringPath, value, expectedExpression);
-		BDDMockito.willReturn(String.class).given(stringPath).getType();
 		BooleanExpressionFactory booleanExpressionFactory = new BooleanExpressionFactory(conversionService);
 
 		// when
@@ -43,19 +49,46 @@ public class BooleanExpressionFactoryTest {
 				List.of(value));
 
 		// then
-		assertThat(expectedExpression == createdExpression);
+		assertThat(createdExpression).isNotNull();
+		verifyFunction.accept(stringPath, value);
+		then(stringPath).shouldHaveNoMoreInteractions();
 		then(conversionService).shouldHaveNoInteractions();
-
-		Mockito.reset(stringPath);
 	}
 
 	private static Stream<Arguments> operatorSymbolSource() {
-		return Stream.of(
-				Arguments.of(OperatorSymbols.EQUAL,
-						(MockFunction<StringPath, String, BooleanExpression>) (t, u, r) -> given(t.eq(u))
-								.willReturn(r)),
-				Arguments.of(OperatorSymbols.NOT_EQUAL, (MockFunction<StringPath, String, BooleanExpression>) (t, u,
-						r) -> given(t.ne(u)).willReturn(r)));
+		return Stream.of(Arguments.of(EQUAL, (MockFunction<StringPath, String, BooleanExpression>) (m, p, r) -> {
+			willReturn(String.class).given(m).getType();
+			given(m.eq(p)).willReturn(r);
+		}, (BiConsumer<StringPath, String>) (m, p) -> {
+			then(m).should().getType();
+			then(m).should().eq(p);
+		}), Arguments.of(NOT_EQUAL, (MockFunction<StringPath, String, BooleanExpression>) (m, p, r) -> {
+			willReturn(String.class).given(m).getType();
+			given(m.ne(p)).willReturn(r);
+		}, (BiConsumer<StringPath, String>) (m, p) -> {
+			then(m).should().getType();
+			then(m).should().ne(p);
+		}), Arguments.of(IN, (MockFunction<StringPath, String, BooleanExpression>) (m, p, r) -> {
+			willReturn(String.class).given(m).getType();
+			given(m.in(List.of(p))).willReturn(r);
+		}, (BiConsumer<StringPath, String>) (m, p) -> {
+			then(m).should().getType();
+			then(m).should().in(List.of(p));
+		}), Arguments.of(NOT_IN, (MockFunction<StringPath, String, BooleanExpression>) (m, p, r) -> {
+			willReturn(String.class).given(m).getType();
+			given(m.notIn(List.of(p))).willReturn(r);
+		}, (BiConsumer<StringPath, String>) (m, p) -> {
+			then(m).should().getType();
+			then(m).should().notIn(List.of(p));
+		}), Arguments.of(LIKE,
+				(MockFunction<StringPath, String, BooleanExpression>) (m, p, r) -> given(m.like(p)).willReturn(r),
+				(BiConsumer<StringPath, String>) (m, p) -> then(m).should().like(p)),
+				Arguments.of(LIKE_CASE_INSENSITIVE,
+						(MockFunction<StringPath, String, BooleanExpression>) (m, p, r) -> given(m.likeIgnoreCase(p))
+								.willReturn(r),
+						(BiConsumer<StringPath, String>) (m, p) -> then(m).should().likeIgnoreCase(p))
+
+		);
 	}
 
 }
