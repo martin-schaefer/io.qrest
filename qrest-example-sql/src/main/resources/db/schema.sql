0cba4d82232f01f0c0d10bac3bfd1d01f03fa6dd
Martin Schäfer <martin.schaefer.14@outlook.com>
2021-08-13 20:00:32 +0200

    Moved test data to schema.sql

--- qrest-example-sql/src/main/resources/db/schema.sql
+++ qrest-example-sql/src/main/resources/db/schema.sql
@@ -24,6 +24,19 @@ create table Contract (
 	number varchar(64) not null,
 	premium numeric(9,2) not null,
 	inceptionDate date,
+	signingTime timestamp,
 	insuredPerson varchar(64) not null,
 	foreign key (insuredPerson) references Person(id)
 	);
+	
+insert into Person(id,firstName,lastName) values('person0', 'Max', 'Maier');
+insert into Person(id,firstName,lastName) values('person1', 'Sandra', 'Müller');
+insert into Person(id,firstName,lastName) values('person2', 'Karl', 'Feinmann');
+insert into Person(id,firstName,lastName) values('person3', 'Diana', 'Reinmann');
+insert into Contract(id,number,premium,inceptionDate,signingTime,insuredPerson) values('contract0', 'T34567', 1220.05, parsedatetime('31-12-2018', 'dd-MM-yyyy'), parsedatetime('30-12-2018 16:30:19', 'dd-MM-yyyy HH:mm:ss'),'person0');
+insert into Contract(id,number,premium,inceptionDate,signingTime,insuredPerson) values('contract1', 'T34568', 130.05, parsedatetime('12-10-2019', 'dd-MM-yyyy'), parsedatetime('02-10-2019 15:10:23', 'dd-MM-yyyy HH:mm:ss'),'person1');
+insert into Contract(id,number,premium,inceptionDate,signingTime,insuredPerson) values('contract2', 'T34569', 34.05, parsedatetime('22-12-2020', 'dd-MM-yyyy'), parsedatetime('22-11-2020 11:55:00', 'dd-MM-yyyy HH:mm:ss'),'person2');
+insert into Contract(id,number,premium,inceptionDate,signingTime,insuredPerson) values('contract3', 'T44570', 230.05, parsedatetime('23-04-2021', 'dd-MM-yyyy'), parsedatetime('05-04-2021 07:05:30', 'dd-MM-yyyy HH:mm:ss'),'person3');
+insert into Contract(id,number,premium,inceptionDate,signingTime,insuredPerson) values('contract4', 'T44571', 2095.00, parsedatetime('01-02-2021', 'dd-MM-yyyy'), parsedatetime('01-02-2021 16:30:19', 'dd-MM-yyyy HH:mm:ss'),'person1');
+insert into Contract(id,number,premium,inceptionDate,signingTime,insuredPerson) values('contract6', 'T44572', 367.50, parsedatetime('10-10-2019', 'dd-MM-yyyy'), parsedatetime('02-10-2019 09:58:00', 'dd-MM-yyyy HH:mm:ss'),'person3');
+	
