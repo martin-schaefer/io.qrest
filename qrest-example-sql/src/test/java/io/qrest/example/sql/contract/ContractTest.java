24baf0c584bc80223a30bbe930389823e39e9ce5
Martin Schäfer <martin.schaefer.14@outlook.com>
2021-08-14 23:51:40 +0200

    added more where clause tests

--- qrest-example-sql/src/test/java/io/qrest/example/sql/contract/ContractTest.java
+++ qrest-example-sql/src/test/java/io/qrest/example/sql/contract/ContractTest.java
@@ -46,8 +46,16 @@ public class ContractTest {
 				Arguments.of("premium==367.50", "contract5"),
 				Arguments.of("premium==367.50", "contract5"),
 				Arguments.of("insuredperson==person0", "contract0"),
-				Arguments.of("number==T34567", "contract0")
-
-		);
+				Arguments.of("number==T34567", "contract0"),
+				Arguments.of("premium=ge=367.50 and premium=le=368.00", "contract5"),
+				Arguments.of("number=in=(T34567,T94567)", "contract0"),
+				Arguments.of("number=out=(T34567,T34568,T34569,T44570,T44572)", "contract4"),
+				Arguments.of("inceptiondate=in=(2019-10-12,2019-10-13,2019-10-14)", "contract1"),
+				Arguments.of("id=in=(contract3,contract99)", "contract3"),
+				Arguments.of("number!=T44572 and insuredperson==person3", "contract3"),
+				Arguments.of("number=lk=T44* and number!=T44570 and number!=T44571", "contract5"),
+				Arguments.of("number=lki=T44* and number!=T44570 and number!=T44571", "contract5"),
+				Arguments.of("signingtime=gt=2020-11-22T11:54:30 and signingtime=lt=2020-11-22T11:55:01", "contract2"),
+				Arguments.of("comments==null", "contract5"));
 	}
 }
